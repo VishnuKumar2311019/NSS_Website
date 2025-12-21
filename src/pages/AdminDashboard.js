ab063184cabc44616a1507c0dd6ffcfed1a620e2
VISHNU KUMAR S <vishnukumar2311019@ssn.edu.in>
2025-12-21 09:51:47 +0530

    Add Navbar component and update admin dashboard

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -37,7 +37,7 @@ const AdminDashboard = () => {
   useEffect(() => {
   if (activeTab === 'members' && selectedAction === 'view') {
     console.log("Fetching users...");
-    fetch("http://127.0.0.1:5000/admin/get-users", {
+    fetch("https://nss-website-backend.onrender.com/admin/get-users", {
       headers: getAuthHeaders()
     })
       .then((res) => {
