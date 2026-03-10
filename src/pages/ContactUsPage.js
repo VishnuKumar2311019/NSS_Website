d842dc5f0711bbcc4143510b8018e65f3ec5bc4e
Raja_byte <pmaharajacs7@gmail.com>
2026-03-10 22:57:04 +0530

    update contact

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -92,20 +92,20 @@ const ContactUsPage = () => {
             <p><strong>Location:</strong> SSN College of Engineering, Kalavakkam – 603110</p>
           </div>
 
-          <div className="social-icons">
-            <a href="https://www.instagram.com/your_nss_page" target="_blank" rel="noreferrer">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="https://www.youtube.com/@your_channel" target="_blank" rel="noreferrer">
-              <i className="fab fa-youtube"></i>
-            </a>
-            <a href="https://twitter.com/your_handle" target="_blank" rel="noreferrer">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="https://facebook.com/your_facebook_page" target="_blank" rel="noreferrer">
-              <i className="fab fa-facebook"></i>
-            </a>
-          </div>
+         <div className="social-icons">
+  <a href="https://instagram.com/nssofssn" target="_blank" rel="noreferrer">
+    <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" />
+  </a>
+  <a href="https://www.youtube.com/@nssofssn" target="_blank" rel="noreferrer">
+    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png" alt="YouTube" />
+  </a>
+  <a href="https://x.com/SSNNSS579516641" target="_blank" rel="noreferrer">
+    <img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter" />
+  </a>
+  <a href="https://www.facebook.com/nssofssn" target="_blank" rel="noreferrer">
+    <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" />
+  </a>
+</div>
         </div>
       </div>
 
