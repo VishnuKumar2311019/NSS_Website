10d35cf7f92cb3cf450b7294d8c069672c150fdf
Raja_byte <pmaharajacs7@gmail.com>
2026-03-10 23:16:55 +0530

    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -99,25 +99,21 @@ const Home = () => {
       <div className="footer-social">
         <h3>Follow Us</h3>
 <div className="floating-social">
-  <a href="https://instagram.com/nssofssn" target="_blank" rel="noreferrer">
-    <img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram" />
-  </a>
-
-  <a href="https://www.youtube.com/@nssofssn" target="_blank" rel="noreferrer">
-    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png" alt="YouTube" />
-  </a>
-
-  <a href="https://x.com/SSNNSS579516641" target="_blank" rel="noreferrer">
-    <img src="https://cdn-icons-png.flaticon.com/512/5968/5968958.png" alt="Twitter/X" />
-  </a>
-
-  <a href="https://www.facebook.com/nssofssn" target="_blank" rel="noreferrer">
-    <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook" />
-  </a>
-
-  <a href="https://www.linkedin.com/company/nssssn/" target="_blank" rel="noreferrer">
-    <img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt="LinkedIn" />
-  </a>
+<a href="https://instagram.com/nssofssn" target="_blank" rel="noreferrer">
+<img src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png" alt="Instagram"/>
+</a>
+<a href="https://www.youtube.com/@nssofssn" target="_blank" rel="noreferrer">
+<img src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png" alt="YouTube"/>
+</a>
+<a href="https://x.com/SSNNSS579516641" target="_blank" rel="noreferrer">
+<img src="https://cdn-icons-png.flaticon.com/512/5968/5968958.png" alt="Twitter"/>
+</a>
+<a href="https://www.facebook.com/nssofssn" target="_blank" rel="noreferrer">
+<img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook"/>
+</a>
+<a href="https://www.linkedin.com/company/nssssn/" target="_blank" rel="noreferrer">
+<img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt="LinkedIn"/>
+</a>
 </div>
       </div>
 
